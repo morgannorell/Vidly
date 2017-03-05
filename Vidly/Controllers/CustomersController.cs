1b5895759c25ef4e5e3c7027a447e1487c47a5f9
morgannorell <mnorell@live.se>
2017-03-05 22:41:35 +0100

    Vidly app upload

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customer
+        List<Customer> customerList = new List<Customer>
+        {
+            new Customer { Id = 1, Name = "Morgan Norell" },
+            new Customer { Id = 2, Name ="Therese Sallander" },
+            new Customer { Id = 3, Name = "Isac Norell" },
+            new Customer { Id = 4, Name = "Emma Norell"  },
+            new Customer { Id = 5, Name = "Elise Jägrup Sallander" }
+        };
+
+        public ActionResult Index()
+        {           
+            var ViewModel = new CustomerViewModel
+            {
+                Customers = customerList
+            };
+
+            return View(ViewModel);
+        }
+
+        [Route("Customers/Detail/{Id}")]
+        public ActionResult Details(int id)
+        {
+            Movie customer = new Movie();
+
+            foreach (var c in customerList)
+            {
+                if (c.Id == id)
+                    customer.Name = c.Name;       
+            }
+
+            return View(customer);          
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,17 +4,71 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        List<Movie> movieList = new List<Movie>
+        {
+            new Movie { Id = 1, Name = "Shrek" },
+            new Movie { Id = 2, Name = "Star Wars" },
+            new Movie { Id = 3, Name = "Star Trek" },
+            new Movie { Id = 4, Name = "Batman Returns"  },
+            new Movie { Id = 5, Name = "Arrow" }
+        };
+
         // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Movie>
+            {
+                new Movie { Name = "Morgan" },
+                new Movie { Name = "Therese" }
+            };
 
-            return View(movie);
+            var ViewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(ViewModel);
         }
+
+        // ASP.NET MVC Attribute Route Constraints
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // movies
+        public ActionResult Index()
+        {
+            var ViewModel = new MovieViewModel
+            {
+                movies = movieList
+            };
+
+            return View(ViewModel);
+        }
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+
+        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //}
     }
 }
